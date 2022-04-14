26aaa2e38d9964e343431e9c655b6c842867922b
Gabriel Revelo <revelo.gabriel.38@gmail.com>
2022-04-14 17:27:29 -0500

    Proyecto funcionando con componentes de clases

---
+++ shopping-cart/src/App.js
@@ -0,0 +1,61 @@
+import { Component } from 'react'
+import Productos from './components/Productos'
+import Layout from './components/Layout'
+import Navbar from './components/Navbar'
+import Title from './components/Title'
+
+class App extends Component {
+  state = {
+    productos: [
+      { name: 'Tomate', price: 1500, img: '/productos/tomate.jpg' },
+      { name: 'Arbejas', price: 2500, img: '/productos/arbejas.jpg' },
+      { name: 'Lechuga', price: 500, img: '/productos/lechuga.jpg' }
+    ],
+    carro: [],
+    esCarroVisible: false
+  }
+
+  agregarAlCarro = (producto) => {
+    const { carro } = this.state
+    if (carro.find(e => e.name === producto.name)) {
+      const newCarro = carro.map(e => e.name === producto.name
+        ? ({
+          ...e,
+          cantidad: e.cantidad + 1
+        })
+        : e)
+      return this.setState({ carro: newCarro })
+    }
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad: 1
+      })
+    })
+  }
+
+  mostrarCarro = () => {
+    if(!this.state.carro.length) {
+      return
+    }
+    this.setState({ esCarroVisible: !this.state.esCarroVisible })
+  }
+
+  render() {
+    const {esCarroVisible} = this.state
+    return (
+      <div>
+        <Navbar carro={this.state.carro} esCarroVisible={esCarroVisible} mostrarCarro={this.mostrarCarro}/>
+        <Layout>
+          <Title />
+          <Productos
+            agregarAlCarro={this.agregarAlCarro}
+            productos={this.state.productos}
+          />
+        </Layout>
+      </div>
+    )
+  }
+}
+
+export default App;
